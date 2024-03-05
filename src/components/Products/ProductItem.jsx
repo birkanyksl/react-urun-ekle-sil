2feba6c2f3b13ae53c215a5b64bf614e53aa9604
Birkan <birkanyksl613@gmail.com>
2024-03-05 20:55:18 +0300

    Ürün ekleme formu oluşturuldu

--- src/components/Products/ProductItem.jsx
+++ src/components/Products/ProductItem.jsx
@@ -1,23 +1,27 @@
-import React from "react";
-
+import React, { useState } from "react";
 import "./ProductItem.css";
 import Counter from "./Counter";
 
 const ProductItem = ({ product }) => {
-  const { imageUrl, productName} = product;
+  const { imageUrl, productName } = product;
+  const [title, setTitle] = useState(productName);
+
+  const clickHandler = () => {
+    setTitle("Güncellendi");
+  };
 
   return (
     <div className="product-item">
       <div className="product-image">
         <img src={imageUrl} alt="" />
       </div>
-      <div className="product-info">{productName}</div>
+      <div className="product-info">{title}</div>
       <div className="price">
         <span>
-          <Counter product={product}/>
+          <Counter product={product} />
         </span>
       </div>
-      <button>Güncelle</button>
+      <button onClick={clickHandler}>Güncelle</button>
     </div>
   );
 };
