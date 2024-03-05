66441b38ef6c83efcf17b4ef66f2a43c1c048989
Birkan <birkanyksl613@gmail.com>
2024-03-05 22:00:42 +0300

    Update ProductForm.jsx
    inputlar için state oluşturuldu. onSubmit eklendi.

--- src/components/NewProducts/ProductForm.jsx
+++ src/components/NewProducts/ProductForm.jsx
@@ -1,22 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProductForm.css";
 
 const ProductForm = () => {
+
+  const [productName, setProductName]=useState("")
+  const [productPrice, setProductPrice]=useState("")
+  const [imageUrl, setImageUrl]=useState("")
+
+  const titleChangeHandler = (event) => {
+  setProductName(event.target.value);
+  }
+
+  const priceChangeHandler = (event) => {
+    setProductPrice(event.target.value);
+    }
+    
+
+    const imageChangeHandler = (event) => {
+      setImageUrl(event.target.value);
+      }
+
+      const submitHandler = (event) => {
+       event.preventDefault()
+        
+       const newProductData = {
+        productName,
+        productPrice,
+        imageUrl
+       }
+       console.log(newProductData);
+      }
+   
   return (
-    <form className="product-form">
+    <form className="product-form" onSubmit={submitHandler}>
       <div className="product-form-input">
         <label >Ürün Adı</label>
-        <input type="text" placeholder="Ürün adı giriniz."/>
+        <input type="text" placeholder="Ürün adı giriniz." onChange={titleChangeHandler}/>
       </div>
 
       <div className="product-form-input">
         <label >Ürün Fiyatı</label>
-        <input type="number" placeholder="Ürün fiyatı giriniz."/>
+        <input type="number" placeholder="Ürün fiyatı giriniz." onChange={priceChangeHandler}/>
       </div>
 
       <div className="product-form-input">
         <label >Ürün Görseli</label>
-        <input type="text" placeholder="Ürün görseli giriniz."/>
+        <input type="text" placeholder="Ürün görseli giriniz." onChange={imageChangeHandler}/>
       </div>
       <button className="product-form-button">Ürün Ekle</button>
     </form>
